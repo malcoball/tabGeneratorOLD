59aa48f1c8b199402e271413e8234ac893ee4d90
AdamShaw <59421635+malcoball@users.noreply.github.com>
2023-08-09 08:04:49 +0100

    \delete

--- src/Tone/instrumentClass.ts
+++
@@ -1,7 +0,0 @@
-export class Instrument {
-    synth : any; title : string;
-    constructor(synth:any,title:string){
-        this.synth = synth;
-        this.title = title;
-    }
-}
--- src/Tone/synths.ts
+++
@@ -1,9 +0,0 @@
-import * as Tone from 'tone';
-import { Instrument } from './instrumentClass';
-
-const synths  :any =  []
-synths.push(new Instrument(new Tone.Synth().toDestination(),"synth1"));
-synths.push(new Instrument(new Tone.FMSynth().toDestination(),"synth2"));
-synths.push(new Instrument(new Tone.AMSynth().toDestination(),"synth3"));
-
-export default synths;
